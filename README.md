1aa00b118d6d919ce1d9f55d779c8e605306b57e
chr1st1ank <chr1st1ank@krudewig-online.de>
2019-04-07 15:25:02 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,125 @@
 # floorplan-card
-A custom interactive svg element for the home-assistant lovelace UI
+This is a card for [Lovelace](https://www.home-assistant.io/lovelace) on [Home Assistant](https://www.home-assistant.io/)
+which allows to dynamically render an SVG graphic, for example for a floorplan.
+It is a replacement for pkozul's great [ha-floorplan](https://github.com/pkozul/ha-floorplan) which sadly no longer
+works under the new Lovelace UI.
+
+## Installation
+
+### Step 1: Get the code
+
+Save the following files from this repository to `<config directory>/www/` on your Home Assistant instance:
+
+- floorplan-card.js
+- floorplan-card-style.js
+- example-floorplan.svg
+
+
+### Step 2: Activate the custom card
+
+Link `floorplan-card` inside your `ui-lovelace.yaml` (or in the 'raw configuration' in the UI).
+
+```yaml
+resources:
+  - url: /local/floorplan-card.js
+    type: js
+```
+
+
+### Step 3: Add an SVG drawing
+
+Create an SVG file, e.g. with a floorplan. For this, for example
+(Inkscape)[https://inkscape.org/en/develop/about-svg/] can be used.
+For each Home Assistant entity to be displayed on the floorplan,
+create one svg element (e.g. a rectangle, text or group).
+Then set the `id` of this element to the full entity id of
+Home Assistant.
+
+In the markup code of the svg file, this would for example look like
+(only opening tag):
+```svg
+<g id="switch.example_switch" inkscape:label="#g77286" transform="translate(2.1166667,-3.7041667)">
+```
+
+
+### Step 4: Configure the custom card
+
+Add a custom element in your `ui-lovelace.yaml` or add it via the UI editor.
+Then define all entities which should be styled in the svg picture.
+Their styles can be specified with css classes and also a "toggle" action for
+lights is supported.
+Sensor values can be printed into the svg picture.
+
+Here is an example of a configuration with all supported functionalities:
+
+```yaml
+type: 'custom:floorplan-card'
+title: Example Floorplan
+entity: fp_example
+image: /local/example-floorplan.svg
+stylesheet: /local/floorplan-card-style.css
+
+groups:
+  # Example for sensor's. Their current value and if applicable unit
+  # of measurement will replace the text inside the corresponding
+  # svg elements.
+  - name: Sensors
+    entities:
+      - sensor.owm_temperature
+      - sensor.owm_cloud_coverage
+      - sensor.owm_humidity
+    on_update: 'set_text'
+
+  # Example for lights. Svg elements with their name will be styled
+  # according to the mentioned class as it is specified in the
+  # css file.
+  # In addition the lights can be switched on and off by clicking
+  # on them or by tapping on them in case of a touchscreen.
+  - name: LightSwitches
+    entities:
+       - switch.sample_switch
+       - light.sample_light
+    states:
+      - state: 'on'
+        class: 'light-on'
+      - state: 'off'
+        class: 'light-off'
+    action:
+      service: toggle
+
+  # Example for window sensors. Svg elements with their name will be styled
+  # according to the mentioned class as it is specified in the
+  # css file.
+  - name: Windows
+    entities:
+      - binary_sensor.sample_windows
+    states:
+      - state: 'off'
+        class: 'info-background'
+      - state: 'on'
+        class: 'open-window'
+
+  # Example for movement detection. Svg elements with their name will be styled
+  # according to the mentioned class as it is specified in the
+  # css file.
+  - name: Movement
+    entities:
+      - binary_sensor.sample_pir
+    states:
+      - state: 'on'
+        class: 'movement-bg'
+```
+
+A full example config is contained in [example-floorplan.yaml](example-floorplan.yaml).
+
+
+## Replacing ha-floorplan
+This custom card was written as a replacement for [ha-floorplan](https://github.com/pkozul/ha-floorplan).
+It focuses on the core functionality, though.
+So if you only use ha-floorplan for assigning state-dependent css classes e.g. to
+assign a different color to opened or closed windows or illuminated lights or to
+print sensor values onto the picture, you can use this custom card as a drop-in replacement.
+Simply take your floorplan/svg file and the css styles. The configuration might be
+needed to adjusted slightly, but most things should stay the same.
+One shortcomming is that jinja template literals are not supported. Instead
+you have to use 'on_update: set_text' tags in the yaml file.
---
+++ floorplan-card.js
@@ -0,0 +1,158 @@
+
+class ActiveDrawing extends HTMLElement {
+
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+  }
+
+  updateStates() {
+    const svg_doc = this.image.getSVGDocument();
+    if(svg_doc && this.myHass)
+    {
+        var states = this.myHass.states;
+
+        // For each group of the card's config
+        this.config.groups.forEach(function(group)
+        {
+          // If the group has a "states" item
+          if("states" in group)
+          {
+              // Create a dictionary of state-dependent styles
+              var style_map = new Map();
+              group.states.forEach(function(state_obj)
+              {
+                style_map.set(state_obj.state, state_obj.class);
+              });
+
+              // For each entity set the style according to the state
+              group.entities.forEach(function(entity_id)
+              {
+                const state = states[entity_id];
+                const state_str = state ? state.state : 'unavailable';
+                var style_class = style_map.get(state_str);
+                style_class = style_class ? style_class : 'default';
+                var entity = svg_doc.getElementById(entity_id);
+                if(entity){
+                    entity.setAttribute("class", style_class);
+                }
+              });
+          }
+          // If the group has on_update: 'set_text'
+          if("on_update" in group && group.on_update == "set_text")
+          {
+              // For each entity write the current status into the svg element
+              group.entities.forEach(function(entity_id)
+              {
+                // Get entity and state
+                const entity = states[entity_id];
+                var state_str = entity.state;
+                // If there is a unit of measurement, append it
+                var unit = entity["attributes"]["unit_of_measurement"];
+                if(unit){
+                    state_str = state_str + unit;
+                }
+                // Get svg element and write text
+                var element = svg_doc.getElementById(entity_id);
+                if(element){
+                    element.textContent = state_str;
+                }
+              });
+          }
+        });
+    }
+  }
+
+  prepareSVGOnceLoaded() {
+    // Add the stylesheet to the svg file
+    var svg_doc = this.image.contentDocument;
+    var style = svg_doc.createElementNS("http://www.w3.org/2000/svg", "style");
+    // Make the browser load our css
+    style.textContent = '@import url("' + this.config.stylesheet + '");';
+    var svgElem = svg_doc.querySelector('svg');
+    svgElem.insertBefore(style, svgElem.firstChild);
+
+    // Set onClick handlers
+    this.config.groups.forEach(group => { // For each group of the card's config
+      // If the group has an "entities" item
+      if("entities" in group && "action" in group)
+      {
+          // For each entity set the onClick handler when an action is defined
+          group.entities.forEach(entity_id => {
+            var domain = entity_id.split('.')[0];
+            var svg_element = svg_doc.getElementById(entity_id);
+            if(group.action.service == "toggle"){
+                // Click event for mouse
+                svg_element.addEventListener('click', this.myHass.callService.bind(
+                    this.myHass, domain, 'toggle', {
+                      "entity_id": entity_id
+                }))
+                // Touch event for touchscreens
+                svg_element.addEventListener('touchend', this.myHass.callService.bind(
+                    this.myHass, domain, 'toggle', {
+                      "entity_id": entity_id
+                }))
+            }
+          });
+      }
+    });
+
+    // Already update the states if possible
+    this.updateStates();
+  }
+
+  setConfig(config) {
+    if (!config.entity) {
+      throw new Error('You need to define an entity');
+    }
+    if (!config.image) {
+      throw new Error('You need to define an svg image');
+    }
+    if (!config.stylesheet) {
+      throw new Error('You need to define a stylesheet');
+    }
+    this.config = config;
+
+    if(!this.content)
+    {
+      // Create basic structure of the card
+      const root = this.shadowRoot;
+      this.card = document.createElement('fp-card');
+      this.card.header = 'floorplan-card';
+      this.content = document.createElement('div');
+      this.content.style.padding = '0 16px 16px';
+
+      // Add svg file to card and insert css style once loaded
+      this.image = document.createElement('object');
+      this.image.type = 'image/svg+xml';
+      this.image.data = this.config.image;
+      this.image.style = 'width: 100%';
+      this.image.id = "fp-image";
+      this.image.async = false;
+      this.image.onload = this.prepareSVGOnceLoaded.bind(this);
+
+      // Put all the HTML elements together and also add the title if available
+      this.content.appendChild(this.image);
+      this.card.appendChild(this.content);
+      if(this.config.title){
+        const title = document.createElement('h1');
+        title.innerHTML = this.config.title;
+        root.appendChild(title);
+      }
+      root.appendChild(this.card);
+    }
+  }
+
+  set hass(hass) {
+    // Store the hass object for use in other functions
+    this.myHass = hass;
+
+    // Update states on the svg picture
+    this.updateStates();
+  }
+
+  getCardSize() {
+    return 8;
+  }
+}
+customElements.define('floorplan-card', ActiveDrawing);
